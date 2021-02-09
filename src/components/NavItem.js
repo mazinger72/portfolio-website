fe7d3f006db11cc3382dcd63e71852bca17cf255
Rick Barros <rickbarros@icloud.com>
2021-02-08 20:23:28 -0500

    Update NavItem.js

--- src/components/NavItem.js
+++ src/components/NavItem.js
@@ -5,13 +5,12 @@ import {
 
 class Navitem extends Component {
     render() {
-                return (
-                    <li id={this.props.item}>
-                    <Link to={this.props.tolink} onClick={this.props.activec.bind(this,this.props.item)}>{this.props.item}</Link>
-                    </li>
-                    )
-            }
-        }
-        
-        export default Navitem;
-        
+        return (
+            <li id={this.props.item}>
+                <Link to={this.props.tolink} onClick={this.props.activec.bind(this, this.props.item)}>{this.props.item}</Link>
+            </li>
+        )
+    }
+}
+
+export default Navitem;
