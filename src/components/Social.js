ace6abb4b456c102d0afd8c77424bfdc4b742f19
Rick Barros <rickbarros@icloud.com>
2021-02-08 19:56:57 -0500

    updated

--- src/components/Social.js
+++ src/components/Social.js
@@ -3,10 +3,10 @@ class Social extends Component {
     render() {
         return (
             <div class="social">
-                <a href="https://github.com/mazinger72" target="_blank"><i class="fab fa-github"></i></a>
-                <a href="https://www.instagram.com/mazingermac/" target="_blank"><i class="fab fa-instagram"></i></a>
-                <a href="https://www.facebook.com/rick.barros.710/" target="_blank"><i class="fab fa-facebook-f"></i></a>
-                <a href="https://www.linkedin.com/in/rick-barros" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+                <a href="https://github.com/mazinger72" target="_blank" rel="noopener noreferrer"><i class="fab fa-github"></i></a>
+                <a href="https://www.instagram.com/mazingermac/" target="_blank" rel="noopener noreferrer"><i class="fab fa-instagram"></i></a>
+                <a href="https://www.facebook.com/rick.barros.710/" target="_blank" rel="noopener noreferrer"><i class="fab fa-facebook-f"></i></a>
+                <a href="https://www.linkedin.com/in/rick-barros" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin-in"></i></a>
             </div>
         )
     }
