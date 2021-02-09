1f40dcd240bc385d23f7f996cf27423554925040
Rick Barros <rickbarros@icloud.com>
2021-02-08 20:54:14 -0500

    update

--- src/components/Social.js
+++ src/components/Social.js
@@ -7,6 +7,7 @@ class Social extends Component {
                 <a href="https://www.instagram.com/mazingermac/" target="_blank" rel="noopener noreferrer"><i class="fab fa-instagram"></i></a>
                 <a href="https://www.facebook.com/rick.barros.710/" target="_blank" rel="noopener noreferrer"><i class="fab fa-facebook-f"></i></a>
                 <a href="https://www.linkedin.com/in/rick-barros" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin-in"></i></a>
+                <a href="https://twitter.com/mazingermac" target="_blank" rel="noopener noreferrer"><i class="fab fa-twitter"></i></a>
             </div>
         )
     }
