f6df8bf84ff4e15dfd5382982de482686c290cf5
Rick Barros <66322157+mazinger72@users.noreply.github.com>
2022-10-20 10:31:06 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+	$(".social-icons li img").hover(function() {
+        $(this).css("width", "9em");
+        $(this).css("min-height", "100px")
+	}, function() {
+        $(this).css("width", "70px");
+        $(this).css("min-height", "70px")
+	});
+});
+$(document).ready(function() {
+	$(".email li img").hover(function() {
+        $(this).css("width", "9em");
+        $(this).css("min-height", "100px")
+	}, function() {
+        $(this).css("width", "70px");
+        $(this).css("min-height", "70px")
+	});
+});
